5d3fa12fd6616aeda10796687ebe831db195e83c
wendelloliveira <69347119+wendelloliveira@users.noreply.github.com>
2021-11-01 21:17:50 +0000

    adjustment.

--- api/src/main/java/nl/rabobank/service/AccountService.java
+++ api/src/main/java/nl/rabobank/service/AccountService.java
@@ -1,16 +1,8 @@
 package nl.rabobank.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import nl.rabobank.mongo.repository.AccountRepository;
 
 @Service
 public class AccountService {
-
-    @Autowired
-    private AccountRepository accountRepository;
-    
-    
 
 }
