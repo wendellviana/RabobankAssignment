eb0118074c75ba0cd19e9bc22e2029d9eb8b9254
wendelloliveira <69347119+wendelloliveira@users.noreply.github.com>
2021-11-02 14:55:31 +0000

    test code.

---
+++ api/src/main/test/nl/rabobank/controller/AccessControllerTest.java
@@ -0,0 +1,14 @@
+package nl.rabobank.controller;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class AccessControllerTest {
+    @Test
+    public void test(){
+        boolean is = true;
+        Assertions.assertEquals(is, true);
+    }
+
+}
---
+++ api/src/main/test/nl/rabobank/controller/PaymentAccountControllerTest.java
@@ -0,0 +1,7 @@
+package nl.rabobank.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PaymentAccountControllerTest {
+
+}
