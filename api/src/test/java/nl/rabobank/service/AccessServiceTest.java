4dd0899533971724c0a4987c6025039aa41a606f
wendelloliveira <69347119+wendelloliveira@users.noreply.github.com>
2021-11-03 18:39:52 +0000

    l0o

--- api/src/test/java/nl/rabobank/service/AccessServiceTest.java
+++ api/src/test/java/nl/rabobank/service/AccessServiceTest.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-
 public class AccessServiceTest {
 
     @Autowired
